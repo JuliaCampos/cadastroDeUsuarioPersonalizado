4f8fd15c9d40c8e31ee37afdc1f20a625b36bd41
julia <julia.campos31@gmail.com>
2021-09-28 11:32:23 -0300

    Update views.py

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,15 @@
+#tela para criar usuarios
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .models import UsuarioAutenticacao
+from .forms import UsuarioForm
 # Create your views here.
+######################################
+# Criacao de Usuario                 #
+####################################
+class UsuarioRegister(CreateView):
+    template_name = 'cadastros/form.html'
+    form_class = UsuarioForm
+
+    success_url = reverse_lazy('inicio')
