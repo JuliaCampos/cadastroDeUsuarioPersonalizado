7adec28103e7d17ef49f630d865df397208c5fa1
julia <julia.campos31@gmail.com>
2021-09-28 11:33:13 -0300

    Update models.py

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,3 +1,35 @@
+from asyncio import exceptions
+
 from django.db import models
+from cpf_field.models import CPFField
 
 # Create your models here.
+from pycep_correios import get_address_from_cep, WebService, exceptions
+
+from users import forms
+
+
+class UsuarioAutenticacao(models.Model):
+    nome = models.CharField(max_length=255, verbose_name="Nome:")
+    email = models.EmailField(max_length=255, verbose_name="e-mail:", unique=True)
+    pais = models.CharField(max_length=255, verbose_name="País:")
+    estado = models.CharField(max_length=255)
+    municipio = models.CharField(max_length=255, verbose_name="Município:")
+    cep =  models.CharField(max_length=9, verbose_name="CEP:")
+    rua = models.CharField(max_length=255, verbose_name="Rua:")
+    numero = models.CharField(max_length=255, verbose_name="Número:")
+    complemento = models.CharField(max_length=255, verbose_name="Complemento:", null=True)
+    cpf = CPFField('cpf')
+    pis = models.CharField(max_length=255, verbose_name="PIS:", unique=True, null=True)
+
+    class Meta:
+        # o cpf deve ser unico para cada pessoa, pis ou e-mail
+        unique_together = [
+            ['nome', 'cpf',],
+            ['nome', 'pis',],
+            ['nome', 'email',],
+        ]
+
+    def __str__(self):
+        return self.nome, self.email, self.cpf, self.pis
+
